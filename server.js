a2d547cfc579f04b2c402b207fbefbfa2563cac9
kenil007 <30696556+kenil007@users.noreply.github.com>
2017-08-09 23:45:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 app.get('/ui/main.js',function(req,res){
-    res.sendfile(path.join(__dirnae,'ui','main.js'));
+    res.sendfile(path.join(__dirname,'ui','main.js'));
 });
 
 
