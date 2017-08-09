41d521ff6638805dd9fb17c9183431f6b605fa86
kenil007 <30696556+kenil007@users.noreply.github.com>
2017-08-10 00:12:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 console.log('Loaded!');
 var img = document.getElementById('image1');
 img.onclick = function(){
-    img.style.color = 'white';
+    img.style.marginLeft='100px';
     console.log('inside');
 };
