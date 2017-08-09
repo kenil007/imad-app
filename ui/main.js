85e32e80d0fbfdf7a86ce4748263cd9cb9d86c0b
kenil007 <30696556+kenil007@users.noreply.github.com>
2017-08-10 00:28:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,13 +4,13 @@ var margin=0;
 var cut=0;
  function right(){
     img.style.marginLeft = margin+'px';
-   if(margin>100){
+   if(margin>1000){
    cut=1;
     }
      if(cut===0){
    margin = margin +10;
     }
-     if(margin<-100){
+     if(margin<-1000){
    cut=0;
     }
      if(cut==1){
@@ -19,7 +19,7 @@ var cut=0;
  }
 img.onclick = function(){
    
-    var interval = setInterval(right,100);
+    var interval = setInterval(right,10);
     
     console.log('inside');
 };
